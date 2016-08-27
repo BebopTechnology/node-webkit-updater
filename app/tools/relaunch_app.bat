1cdbe364377059e0a30b993629256e3ca142ec44
Nicola De Franceschi <Nicola@MacBook-Pro-di-Nicola-De-Franceschi.local>
2016-08-27 15:32:56 +0200

    Added tools

---
+++ app/tools/relaunch_app.bat
@@ -0,0 +1,16 @@
+@echo off
+rem usage: relaunch_app.bat <seconds to wait> <executable to launch> <arguments>
+rem i.e. $>relaunch_app.bat 5 "C:\Program Files\Jujusoft\JujuEdit\JujuEdit.exe" "C:\Windows\system.ini"
+
+echo Restarting in %1 seconds...
+title Waiting
+
+rem Windows 7
+@timeout /t %1 /nobreak >nul 2>&1
+
+rem Windows XP
+if errorlevel 1 ping 192.0.2.2 -n 1 -w %1000 > nul
+
+rem # launch the executable via start command
+start /B "" %2 %3 %4 %5 %6 %7 %8
+rem # /MIN
---
+++ app/tools/relaunch_app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Please wait for ${1} seconds.."
+sleep $1
+open -n -a "${2}" --args $3 $4 $5 $6 $7 $8 
